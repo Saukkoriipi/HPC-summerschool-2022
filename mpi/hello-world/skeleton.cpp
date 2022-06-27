ad398b9227ef866b53dd400232428ae01efb9608
Saukkoriipi <saukkoriipi.mikko@gmail.com>
2022-06-27 11:55:42 +0300

    Cleaning

--- mpi/hello-world/skeleton.cpp
+++ mpi/hello-world/skeleton.cpp
@@ -1,7 +1,16 @@
 #include <cstdio>
+# include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    // TODO: say hello! in parallel
+   int i, myid, ntasks, namelen;
+   char procname[MPI_MAX_PROCESSOR_NAME];   
 
+   MPI_Init(&argc, &argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+   MPI_Get_processor_name(procname, &namelen);
+
+   printf("Hello\n");
+   MPI_Finalize();
 }
