a20c814c34ec0a7ebed9461f9a69846b8d91c95b
Saukkoriipi <saukkoriipi.mikko@gmail.com>
2022-07-01 15:00:05 +0300

    Add answers

--- gpu-openmp/dot-product/dot-product.c
+++ gpu-openmp/dot-product/dot-product.c
@@ -6,20 +6,17 @@ int main(void)
 {
     double vecA[NX], vecB[NX];
 
-    // Initialization of the vectors
+    /* Initialization of the vectors */
     for (int i = 0; i < NX; i++) {
         vecA[i] = 1.0 / ((double) (NX - i));
         vecB[i] = vecA[i] * vecA[i];
     }
 
-    // TODO start: offload and parallelize the computation
-
     double res = 0.0;
+#pragma omp target teams distribute parallel for map(to:vecA,vecB) reduction(+:res)
     for (int i = 0; i < NX; i++) {
         res += vecA[i] * vecB[i];
     }
-
-    // TODO end
 
     printf("Dot product: %18.16f\n", res);
 
--- gpu-openmp/sum-dot/sum-dot.c
+++ gpu-openmp/sum-dot/sum-dot.c
@@ -12,20 +12,21 @@ int main(void)
         vecB[i] = vecA[i] * vecA[i];
     }
 
-    // TODO start: create a data region and offload the two computations
-    // so that data is kept in the device between the computations
+    double res = 0.0;
 
+#pragma omp target data map(to:vecA, vecB) map(from:vecC, res)
+  {
+#pragma omp target teams distribute parallel for
     for (int i = 0; i < NX; i++) {
         vecC[i] = vecA[i] + vecB[i];
     }
 
-    double res = 0.0;
-
+#pragma omp target teams distribute parallel for reduction(+:res)
     for (int i = 0; i < NX; i++) {
         res += vecC[i] * vecB[i];
     }
 
-    // TODO end
+  }
 
     double sum = 0.0;
     /* Compute the check value */
